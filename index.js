8c4ad6715ca55ccce81006eaaa969c18665738b5
srib1997 <srib1997@yahoo.com.hk>
2018-12-16 11:29:17 +0800

    First commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var cv = require('opencv4nodejs')
-var FPS = 10
-var wCameraWidth = cv.CAP_PROP_FRAME_WIDTH
-var wCameraHeight = cv.CAP_PROP_FRAME_HEIGHT
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var cv = require('opencv4nodejs')
+var FPS = 10
 var wCap0 = new cv.VideoCapture(0)
 var wCap1 = new cv.VideoCapture(1)
 var wCap2 = new cv.VideoCapture(2)
 var wCap3 = new cv.VideoCapture(3)
-var wCap4 = new cv.VideoCapture(4)
-var wCap5 = new cv.VideoCapture(5)
-var wCap6 = new cv.VideoCapture(6)
-var wCap7 = new cv.VideoCapture(7)
+// var wCap4 = new cv.VideoCapture(4)
+// var wCap5 = new cv.VideoCapture(5)
+// var wCap6 = new cv.VideoCapture(6)
+// var wCap7 = new cv.VideoCapture(7)
+var wCameraWidth = cv.CAP_PROP_FRAME_WIDTH
+var wCameraHeight = cv.CAP_PROP_FRAME_HEIGHT
 
 app.use(express.static('./public'))
 
@@ -29,17 +29,17 @@ wCap2.set(wCameraHeight, 300)
 wCap3.set(wCameraWidth, 300)
 wCap3.set(wCameraHeight, 300)
 
-wCap4.set(wCameraWidth, 300)
-wCap4.set(wCameraHeight, 300)
+// wCap4.set(wCameraWidth, 300)
+// wCap4.set(wCameraHeight, 300)
 
-wCap5.set(wCameraWidth, 300)
-wCap5.set(wCameraHeight, 300)
+// wCap5.set(wCameraWidth, 300)
+// wCap5.set(wCameraHeight, 300)
 
-wCap6.set(wCameraWidth, 300)
-wCap6.set(wCameraHeight, 300)
+// wCap6.set(wCameraWidth, 300)
+// wCap6.set(wCameraHeight, 300)
 
-wCap7.set(wCameraWidth, 300)
-wCap7.set(wCameraHeight, 300)
+// wCap7.set(wCameraWidth, 300)
+// wCap7.set(wCameraHeight, 300)
 
 setInterval(() => {
   const frame = wCap0.read()
@@ -56,23 +56,23 @@ setInterval(() => {
 
   const frame3 = wCap3.read()
   const image3 = cv.imencode('.jpg', frame3).toString('base64')
-  io.emit('image3', image3)
+  io.emit('image2', image3)
 
-  const frame4 = wCap4.read()
-  const image4 = cv.imencode('.jpg', frame4).toString('base64')
-  io.emit('image4', image4)
+  // const frame4 = wCap4.read()
+  // const image4 = cv.imencode('.jpg', frame4).toString('base64')
+  // io.emit('image4', image4)
 
-  const frame5 = wCap5.read()
-  const image5 = cv.imencode('.jpg', frame5).toString('base64')
-  io.emit('image5', image5)
+  // const frame5 = wCap5.read()
+  // const image5 = cv.imencode('.jpg', frame5).toString('base64')
+  // io.emit('image5', image5)
 
-  const frame6 = wCap6.read()
-  const image6 = cv.imencode('.jpg', frame6).toString('base64')
-  io.emit('image6', image6)
+  // const frame6 = wCap6.read()
+  // const image6 = cv.imencode('.jpg', frame6).toString('base64')
+  // io.emit('image6', image6)
 
-  const frame7 = wCap7.read()
-  const image7 = cv.imencode('.jpg', frame7).toString('base64')
-  io.emit('image7', image7)
+  // const frame7 = wCap7.read()
+  // const image7 = cv.imencode('.jpg', frame7).toString('base64')
+  // io.emit('image7', image7)
 
 }, 1000 / FPS)
 
@@ -80,3 +80,44 @@ setInterval(() => {
 http.listen(3000, function () {
     console.log('listening on *:' + 3000)
   })
+
+// const cv = require('opencv4nodejs');
+
+// const img = cv.imread('./Lenna.png');
+
+// // single axis for 1D hist
+// const getHistAxis = channel => ([
+//   {
+//     channel,
+//     bins: 256,
+//     ranges: [0, 256]
+//   }
+// ]);
+
+// // calc histogram for blue, green, red channel
+// const bHist = cv.calcHist(img, getHistAxis(0));
+// const gHist = cv.calcHist(img, getHistAxis(1));
+// const rHist = cv.calcHist(img, getHistAxis(2));
+
+// const blue = new cv.Vec(255, 0, 0);
+// const green = new cv.Vec(0, 255, 0);
+// const red = new cv.Vec(0, 0, 255);
+
+// // plot channel histograms
+// const plot = new cv.Mat(300, 600, cv.CV_8UC3, [255, 255, 255]);
+// cv.plot1DHist(bHist, plot, blue, { thickness: 2 });
+// cv.plot1DHist(gHist, plot, green, { thickness: 2 });
+// cv.plot1DHist(rHist, plot, red, { thickness: 2 });
+
+// cv.imshow('rgb image', img);
+// cv.imshow('rgb histogram', plot);
+// cv.waitKey();
+
+// const grayImg = img.bgrToGray();
+// const grayHist = cv.calcHist(grayImg, getHistAxis(0));
+// const grayHistPlot = new cv.Mat(300, 600, cv.CV_8UC3, [255, 255, 255]);
+// cv.plot1DHist(grayHist, grayHistPlot, new cv.Vec(0, 0, 0));
+
+// cv.imshow('grayscale image', grayImg);
+// cv.imshow('grayscale histogram', grayHistPlot);
+// cv.waitKey();
--- public/main.js
+++ public/main.js
@@ -23,23 +23,23 @@ $(function () {
       imageElm3.src = `data:image/jpeg;base64,${image3}`
     })
 
-    socket.on('image4', (image4) => {
-      const imageElm4 = document.getElementById('image4')
-      imageElm4.src = `data:image/jpeg;base64,${image4}`
-    })
+    // socket.on('image4', (image4) => {
+    //   const imageElm4 = document.getElementById('image4')
+    //   imageElm4.src = `data:image/jpeg;base64,${image4}`
+    // })
 
-    socket.on('image5', (image5) => {
-      const imageElm5 = document.getElementById('image5')
-      imageElm5.src = `data:image/jpeg;base64,${image5}`
-    })
+    // socket.on('image5', (image5) => {
+    //   const imageElm5 = document.getElementById('image5')
+    //   imageElm5.src = `data:image/jpeg;base64,${image5}`
+    // })
 
-    socket.on('image6', (image6) => {
-      const imageElm6 = document.getElementById('image6')
-      imageElm6.src = `data:image/jpeg;base64,${image6}`
-    })
+    // socket.on('image6', (image6) => {
+    //   const imageElm6 = document.getElementById('image6')
+    //   imageElm6.src = `data:image/jpeg;base64,${image6}`
+    // })
 
-    socket.on('image7', (image7) => {
-      const imageElm7 = document.getElementById('image7')
-      imageElm7.src = `data:image/jpeg;base64,${image7}`
-    })
+    // socket.on('image7', (image7) => {
+    //   const imageElm7 = document.getElementById('image7')
+    //   imageElm7.src = `data:image/jpeg;base64,${image7}`
+    // })
   })
